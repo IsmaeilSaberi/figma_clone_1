276f9e19ea80e470140e0a7e7542aec993b1983e
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-05-25 10:01:49 -0700

    I add liveblock avatar to the project also1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
+    </main>
   );
 }
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -41,7 +41,10 @@ const CursorChat = ({
         <>
           {" "}
           <CursorSVG color="#000" />
-          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+          <div
+            className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]"
+            onKeyUp={(e) => e.stopPropagation()}
+          >
             {cursorState.previousMessage && (
               <div>{cursorState.previousMessage}</div>
             )}
