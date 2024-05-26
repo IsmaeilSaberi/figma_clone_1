71ae6526daf14006ae0473989d89831e3cd6de2d
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-05-26 10:50:32 -0700

    adding some extra features for drawing any paticular shape!

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   handleCanvasMouseDown,
   handleResize,
   initializeFabric,
 } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -17,6 +18,18 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>("rectangle");
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+
+    selectedShapeRef.current = elem?.value as string;
+  };
 
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef });
@@ -37,7 +50,10 @@ export default function Page() {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
       <section className="flex h-full flex-row">
         <LeftSidebar />
         <Live canvasRef={canvasRef} />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,21 +6,15 @@ import { memo } from "react";
 import { navElements } from "@/constants";
 import { ActiveElement, NavbarProps } from "@/types/type";
 
-// import { Button } from "./ui/button";
-// import ShapesMenu from "./ShapesMenu";
+import { Button } from "./ui/button";
+import ShapesMenu from "./ShapesMenu";
 import ActiveUsers from "./users/ActiveUsers";
-// import { NewThread } from "./comments/NewThread";
+import { NewThread } from "./comments/NewThread";
 
-const Navbar = ({
-  activeElement,
-  imageInputRef,
-  handleImageUpload,
-  handleActiveElement,
-}: NavbarProps) => {
+const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveElement }: NavbarProps) => {
   const isActive = (value: string | Array<ActiveElement>) =>
     (activeElement && activeElement.value === value) ||
-    (Array.isArray(value) &&
-      value.some((val) => val?.value === activeElement?.value));
+    (Array.isArray(value) && value.some((val) => val?.value === activeElement?.value));
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
@@ -35,46 +29,39 @@ const Navbar = ({
               handleActiveElement(item);
             }}
             className={`group px-2.5 py-5 flex justify-center items-center
-            ${
-              isActive(item.value)
-                ? "bg-primary-green"
-                : "hover:bg-primary-grey-200"
-            }
+            ${isActive(item.value) ? "bg-primary-green" : "hover:bg-primary-grey-200"}
             `}
           >
             {/* If value is an array means it's a nav element with sub options i.e., dropdown */}
             {Array.isArray(item.value) ? (
-              <></>
-            ) : //   <ShapesMenu
-            //     item={item}
-            //     activeElement={activeElement}
-            //     imageInputRef={imageInputRef}
-            //     handleActiveElement={handleActiveElement}
-            //     handleImageUpload={handleImageUpload}
-            //   />
-            item?.value === "comments" ? (
-              <></>
-            ) : (
+              <ShapesMenu
+                item={item}
+                activeElement={activeElement}
+                imageInputRef={imageInputRef}
+                handleActiveElement={handleActiveElement}
+                handleImageUpload={handleImageUpload}
+              />
+            ) : item?.value === "comments" ? (
               // If value is comments, trigger the NewThread component
-              //   <NewThread>
-              //     <Button className="relative w-5 h-5 object-contain">
-              //       <Image
-              //         src={item.icon}
-              //         alt={item.name}
-              //         fill
-              //         className={isActive(item.value) ? "invert" : ""}
-              //       />
-              //     </Button>
-              //   </NewThread>
-              <></>
-              //   <Button className="relative w-5 h-5 object-contain">
-              //     <Image
-              //       src={item.icon}
-              //       alt={item.name}
-              //       fill
-              //       className={isActive(item.value) ? "invert" : ""}
-              //     />
-              //   </Button>
+              <NewThread>
+                <Button className="relative w-5 h-5 object-contain">
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    fill
+                    className={isActive(item.value) ? "invert" : ""}
+                  />
+                </Button>
+              </NewThread>
+            ) : (
+              <Button className="relative w-5 h-5 object-contain">
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  fill
+                  className={isActive(item.value) ? "invert" : ""}
+                />
+              </Button>
             )}
           </li>
         ))}
@@ -85,7 +72,4 @@ const Navbar = ({
   );
 };
 
-export default memo(
-  Navbar,
-  (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement
-);
+export default memo(Navbar, (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement);
